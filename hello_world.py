a1041b21fc93c78e4b262b031854024af846ba20
wjsgudrnjs2 <144596221+wjsgudrnjs2@users.noreply.github.com>
2023-09-14 11:11:05 +0900

    Update hello_world.py
    hello_world_2

--- hello_world.py
+++ hello_world.py
@@ -1,6 +1,10 @@
 def print_hello():
     print("Hello World!!")
 
+def print_KIT():
+    print("kumoh National Institute of Technology")
+    
 if __name__ == '__main__':
+    print_KIT()
     print_hello()
     
